95e776eb49188d70157dbd6796735639b4ef6045
Zach Daniel <zach@zachdaniel.dev>
2023-09-25 10:30:21 -0400

    chore: update ash

--- config/config.exs
+++ config/config.exs
@@ -4,8 +4,6 @@ config :logger, level: :error
 
 config :ash, disable_async?: true
 
-config :ash, :use_all_identities_in_manage_relationship?, false
-
 config :ash, :policies,
   show_policy_breakdowns?: true,
   log_policy_breakdowns: :debug,
